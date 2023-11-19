1d97089b4d6dadee99878a75dcfbe10ee37d4221
Doug <mcgidoug@gmail.com>
2023-11-19 15:56:53 -0600

    updated passgen functionality

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+// ========== PASSGEN FUNCTION ==========
+function generatePassword(){
+    const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
+    const upperAlpha = lowerAlpha.toUpperCase();
+    const numbers = '1234567890';
+    const specChar = '!@#$%^&*()-_=+[]{}'
+    let passDraft = lowerAlpha + upperAlpha + numbers + specChar;
+    
+    const passLength = 8;
+    let finalPass = '';
+    for (let i = 0; i < 8; i++) {
+        const randomIndex = Math.floor(Math.random() * passDraft.length);
+        finalPass += passDraft[randomIndex]
+    }
+    return finalPass;
+}
+
 // ========== USER INPUT ==========
 const inputElement = document.createElement('input');
 document.body.appendChild(inputElement)
 
 // ========== PASS GEN BUTTON ==========
-const generatePassword = document.createElement('button');
-document.body.appendChild(generatePassword)
-generatePassword.textContent = 'Generate Password';
+const passGenButton = document.createElement('button');
+document.body.appendChild(passGenButton)
+passGenButton.textContent = 'Generate Password';
+passGenButton.setAttribute('id', 'passGenButton')
+
+// ========== RESET BUTTON ==========
+const resetButton = document.createElement('button');
+document.body.appendChild(resetButton);
+resetButton.textContent = 'RESET';
+resetButton.setAttribute('id', 'resetButton')
 
 // ========== USER INPUT DISPLAY & CHECKING ==========
 let inputDisplay = document.createElement('div');
@@ -14,7 +38,25 @@ inputDisplay.setAttribute('id', 'userInput')
 
 
 // ========== EVENT LISTENERS ==========
+// input div
 inputElement.addEventListener('input', function() {
-    console.log(inputElement.value)
     inputDisplay.textContent = inputElement.value;
-})
+});
+
+// passgen button
+passGenButton.addEventListener('click', function() {
+    passGenButton.style.backgroundColor = 'green'
+    passGenButton.style.color = 'white'
+    passGenButton.textContent = 'PASSWORD GENERATED!'
+    const generatedPassword = generatePassword();
+    console.log(generatedPassword)
+});
+
+// reset button
+resetButton.addEventListener('click', function() {
+    passGenButton.style.backgroundColor = 'aliceBlue'
+    passGenButton.style.color = "black";
+    passGenButton.textContent = 'Generate Password';
+    
+});
+
